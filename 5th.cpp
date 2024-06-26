c5a26e2daab7ab6db4eeefa609d26f83389552d4
Muhammad Burhan <muhammadburhanhaseeb@gmail.com>
2024-06-26 20:49:14 +0500

    ProgramingDay1

---
+++ 5th.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+main()
+{
+
+  cout <<"**********"<<endl;
+
+  cout <<"*        *"<<endl;
+
+  cout <<"*        *"<<endl;
+
+  cout <<"*        *"<<endl;
+
+  cout <<"*        *"<<endl;
+
+  cout <<"*        *"<<endl;
+
+  cout <<"**********"<<endl;
+
+  
+
+
+   
+}
---
+++ first.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+main()
+{
+
+  cout <<"my name is muhammad burhan";
+ 
+}
